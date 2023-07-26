29a0f146ff3c2d951984d7ab52a838f413c4e483
Seweryn Golba <sgoolb@gmail.com>
2023-07-27 00:52:58 +0200

    app update

---
+++ README.md
@@ -0,0 +1,60 @@
+# Mystery Holidays - Destination Randomizer
+
+This is a simple web application called "Mystery Holidays" that allows users to draw random travel destinations with different attributes such as country, city/region, type of travel, and attraction. The app also changes the background image to showcase the chosen destination.
+
+## Getting Started
+
+To use this web application, you can follow these steps:
+
+1. Clone or download the project repository to your local machine.
+
+2. Open the `index.html` file in your web browser.
+
+3. Click on the "Draw a destination" button to get a random travel destination displayed on the screen.
+
+## Features
+
+- **Random Destination:** The application provides a button that, when clicked, selects a random travel destination from the predefined list of destinations and displays its attributes on the screen.
+
+- **Dynamic Background:** When a destination is drawn, the background image of the page changes to showcase an image of the selected destination.
+
+## Technologies Used
+
+The project utilizes the following technologies:
+
+- **HTML:** The structure of the web page.
+- **CSS:** The styles and layout of the elements.
+- **JavaScript:** The logic to handle the random destination selection and updating the page.
+
+## Data Source
+
+The destinations' data is stored in the JavaScript file `main.js` as an array of objects. Each object represents a travel destination with properties like country, city, type, attraction, and photo URL.
+
+```js
+// Example destination object
+{
+  country: "France",
+  city: "Paris",
+  type: "Citybreak",
+  attraction: "Eiffel Tower",
+  photo: "./assets/tower.jpg",
+}
+```
+
+## Customization
+
+You can customize the list of destinations and their attributes by modifying the destinations array in the main.js file. Feel free to add or remove destinations to fit your preferences.
+
+You can also customize the styles of the application by updating the style.css file. The current styles define the layout, colors, and responsiveness for different screen sizes.
+
+## Responsive Design
+
+The web application is designed to be responsive to different screen sizes, thanks to the CSS media queries. The layout and font sizes will adapt to small, medium, and large screens.
+
+## Credits
+
+The background images used in the project are for illustration purposes and are sourced from various image websites like Pexels and Unsplash. Please make sure to replace them with your own images or use royalty-free images if you plan to deploy this project.
+
+## End
+
+Thank you for using the Mystery Holidays web application! Enjoy your random travel destinations!
--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const countrySelect = document.querySelector("#destination-country");
-const citySelect = document.querySelector("#destination-city");
-const travelType = document.querySelector("#destination-type");
-const attractionSelect = document.querySelector("#destination-atrr");
 const drawBtn = document.querySelector("#drawBtn");
+const pageBody = document.querySelector("#page-body");
 
 const destinations = [
   {
@@ -10,5 +7,126 @@ const destinations = [
     city: "Paris",
     type: "Citybreak",
     attraction: "Eiffel Tower",
+    photo: "./assets/tower.jpg",
+  },
+  {
+    country: "Italy",
+    city: "Rome",
+    type: "Citybreak",
+    attraction: "Colosseum",
+    photo: "./assets/colosseum.jpg",
+  },
+  {
+    country: "Poland",
+    city: "Cracow",
+    type: "Citybreak",
+    attraction: "Wawel Castle",
+    photo: "./assets/wawel.jpg",
+  },
+  {
+    country: "Spain",
+    city: "Barcelona",
+    type: "Citybreak",
+    attraction: "Sagrada Familia",
+    photo: "./assets/sagrada.jpg",
+  },
+  {
+    country: "USA",
+    city: "New York",
+    type: "Citybreak",
+    attraction: "Statue of Liberty",
+    photo: "./assets/statue.jpg",
+  },
+  {
+    country: "England",
+    city: "London",
+    type: "Citybreak",
+    attraction: "Big Ben",
+    photo: "./assets/big-ben.jpg",
+  },
+  {
+    country: "Turkey",
+    city: "Cappadocia",
+    type: "Exploring",
+    attraction: "Air Balloon",
+    photo: "./assets/capadocia.jpg",
+  },
+  {
+    country: "Maldives",
+    city: "Male",
+    type: "Sunbathing",
+    attraction: "Beaches",
+    photo: "./assets/maledives.jpg",
+  },
+  {
+    country: "Croatia",
+    city: "Dalmatia",
+    type: "Sunbathing",
+    attraction: "Beaches",
+    photo: "./assets/cro.jpg",
+  },
+  {
+    country: "Italy",
+    city: "Sardinia",
+    type: "Sunbathing",
+    attraction: "Beaches",
+    photo: "./assets/sardegna.jpg",
+  },
+  {
+    country: "Portugal",
+    city: "Faro",
+    type: "Sunbathing",
+    attraction: "Beaches",
+    photo: "./assets/portugal.jpg",
+  },
+  {
+    country: "Japan",
+    city: "Tokyo",
+    type: "Exploring",
+    attraction: "Culture",
+    photo: "./assets/jap.jpg",
+  },
+  {
+    country: "Austria",
+    city: "Alpes",
+    type: "Active",
+    attraction: "Snowboarding",
+    photo: "./assets/alpes.jpg",
+  },
+  {
+    country: "Iceland",
+    city: "Westfjords",
+    type: "Exploring",
+    attraction: "Waterfalls",
+    photo: "./assets/islandia.jpg",
+  },
+  {
+    country: "Thailand",
+    city: "Phuket",
+    type: "Exploring",
+    attraction: "Beaches",
+    photo: "./assets/thailand.jpg",
   },
 ];
+
+const drawDestination = () => {
+  const countrySelect = document.querySelector("#destination-country");
+  const citySelect = document.querySelector("#destination-city");
+  const travelType = document.querySelector("#destination-type");
+  const attractionSelect = document.querySelector("#destination-atrr");
+
+  const randomDestination =
+    destinations[Math.floor(Math.random() * destinations.length)];
+  const { country, city, type, attraction, photo } = randomDestination;
+
+  countrySelect.textContent = `${country}`;
+  citySelect.textContent = `${city}`;
+  travelType.textContent = `${type}`;
+  attractionSelect.textContent = `${attraction}`;
+  pageBody.style.backgroundImage = `url(${photo})`;
+  pageBody.style.backgroundPosition = "center";
+  pageBody.style.backgroundSize = "cover";
+  pageBody.style.backgroundRepeat = "no-repeat";
+};
+
+drawBtn.addEventListener("click", drawDestination);
