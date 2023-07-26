88a06c668d3977c99360f1d913e436293940f7cd
Seweryn Golba <sgoolb@gmail.com>
2023-07-26 23:31:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const countrySelect = document.querySelector("#destination-country");
+const citySelect = document.querySelector("#destination-city");
+const travelType = document.querySelector("#destination-type");
+const attractionSelect = document.querySelector("#destination-atrr");
+const drawBtn = document.querySelector("#drawBtn");
+
+const destinations = [
+  {
+    country: "France",
+    city: "Paris",
+    type: "Citybreak",
+    attraction: "Eiffel Tower",
+  },
+];
